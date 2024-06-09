3b42c5ddf187d1326fbccfa33c97a0cb0131433c
Richard azuogu <s1151084@student.hsleiden.nl>
2024-06-09 17:09:00 +0200

    added function in return service

--- luxuryproducts-frontend/src/app/services/return.service.ts
+++ luxuryproducts-frontend/src/app/services/return.service.ts
@@ -1,27 +1,37 @@
-import {Injectable} from "@angular/core";
-import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Return} from "../models/return.model";
-import {Order} from "../models/order.model";
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Return } from '../models/return.model';
+import { Order } from '../models/order.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReturnService {
-    private baseUrl: string = environment.base_url + "/returns";
-    constructor(private http: HttpClient) { }
+  private baseUrl: string = environment.base_url + '/returns';
+  constructor(private http: HttpClient) {}
 
-    public createReturnRequest(returnRequest: Return) {
-        console.log(returnRequest)
-        return this.http.post(`${this.baseUrl}`, returnRequest, {responseType: 'text'});
-    }
+  public createReturnRequest(returnRequest: Return) {
+    console.log(returnRequest);
+    return this.http.post(`${this.baseUrl}`, returnRequest, {
+      responseType: 'text',
+    });
+  }
 
-    getReturns(): Observable<Return[]> {
-        return this.http.get<Return[]>(`${this.baseUrl}/myReturns`);
-    }
+  getReturns(): Observable<Return[]> {
+    return this.http.get<Return[]>(`${this.baseUrl}/myReturns`);
+  }
 
-    updateReturns(returnStatus: String, id: number) {
-        return this.http.put(`${this.baseUrl}/${id}`, returnStatus, {responseType: 'text'} )
-    }
+  updateReturns(returnStatus: String, id: number) {
+    return this.http.put(`${this.baseUrl}/${id}`, returnStatus, {
+      responseType: 'text',
+    });
+  }
+
+  updateProductReturnStatus(returnid: number, orderid: number) {
+    return this.http.put(`${this.baseUrl}/${returnid}/${orderid}`, {
+      responseType: 'text',
+    });
+  }
 }
